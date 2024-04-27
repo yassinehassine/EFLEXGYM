5d4a59d2e6a3d83464d370f91e07dfa7a2fcee1a
nadazaghdoud <nadazaghdoud.5@gmail.com>
2024-04-27 22:18:24 +0200

    u

--- src/service/EmailSender.php
+++ src/service/EmailSender.php
@@ -1,17 +1,35 @@
 <?php
- 
+
 namespace App\service;
  
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mailer\Transport\Dsn;
- 
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Environment;
+
 class EmailSender
 {
-    public function sendEmail(string $recipientEmail):void
+    private $mailer;
+    private $requestStack;
+    private $urlHelper;
+    private $twig; // Property to hold Twig\Environment service
+
+    public function __construct(MailerInterface $mailer, RequestStack $requestStack, UrlGeneratorInterface $urlGenerator, Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->requestStack = $requestStack;
+        $this->urlHelper = $urlGenerator;
+        $this->twig = $twig; // Inject Twig\Environment service
+    }
+
+    public function sendEmail(string $recipientEmail): void
     {
         // Create a Transport object
+        $request = $this->requestStack->getCurrentRequest();
         $transport = Transport::fromDsn('smtp://nadazaghdoud.5@gmail.com:kczd%20vyvt%20glul%20hzkg@smtp.gmail.com:587');
  
         // Create a Mailer object
@@ -21,11 +39,24 @@ class EmailSender
         $email = (new Email())
             ->from('nadazaghdoud.5@gmail.com')
             ->to($recipientEmail)
-            ->subject('A Cool Subject!')
-            ->text('The plain text version of the message.');
+            ->subject('Votre Abonnement à E-Flex Gym')
+            ->html(
+                $this->renderEmailTemplate('abonnement/email.html.twig', [
+                    // You can pass additional context here if needed
+                ])
+            );
  
         // Send the email
         $mailer->send($email);
     }
+
+    private function renderEmailTemplate(string $template, array $context): string
+    {
+        return $this->twig->render($template, $context);
+    }
+
+    private function getAbsoluteLogoUrl($request, $logoPath)
+    {
+        return $request->getSchemeAndHttpHost() . $this->urlHelper->getAbsoluteUrl($logoPath);
+    }
 }
- 
